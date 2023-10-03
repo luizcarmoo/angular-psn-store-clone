e8dde4e75cc954e0b5ff82fd16a6c466422e1932
Luiz Carmo <luiz.pcarmo@gmail.com>
2023-10-03 00:00:40 -0300

    initial commit

---
+++ src/app/components/card/card.component.ts
@@ -0,0 +1,28 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-card',
+  templateUrl: './card.component.html',
+  styleUrls: ['./card.component.css']
+})
+export class CardComponent implements OnInit {
+
+  @Input()
+  gameCover:string = ""
+  @Input()
+  gameLabel:string = ""
+  @Input()
+  gameType:string = ""
+  @Input()
+  gamePrice:string = "$119.90"
+
+  constructor() {
+
+  }
+
+  ngOnInit(): void {
+    
+  }
+
+}
+
